956926bfbf70b3231b6c369c73ef9ee7278b7f86
Mohamed Ismail <mohamed.ismail@usabilla.com>
2022-09-01 14:13:15 +0200

    feat(header): consume login federated module in header component

--- packages/header/src/components/Header/index.js
+++ packages/header/src/components/Header/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import './index.css';
+
+const Login = React.lazy(() => import("login/App"));
 
 export default function Header() {
   return (
     <div className="header">
       <h2>Header</h2>
+      <Suspense fallback="Loading Login">
+        <Login />
+      </Suspense>
     </div>
   );
 }
