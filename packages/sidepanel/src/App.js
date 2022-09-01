250f1ea1cfcda63c630f10a36fc76ab51fe65109
Mohamed Ismail <mohamed.ismail@usabilla.com>
2022-09-01 15:35:29 +0200

    feat(sidepanel): enable children props to render content from consume component

--- packages/sidepanel/src/App.js
+++ packages/sidepanel/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './index.css';
 
-export default function App() {
+export default function App({ children }) {
   return (
     <div className="sidePanel">
       <h2>Side Panel</h2>
+      {children}
     </div>
   );
 }
